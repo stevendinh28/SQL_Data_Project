bfba322097b03957ce3fc7b316cd287d9cd34573
Steven Dinh <stevendinh.28@gmail.com>
2026-06-05 09:58:50 -0700

    first commit

---
+++ advanced_sql/04_subqueries.sql
@@ -0,0 +1,13 @@
+SELECT 
+    company_name AS name
+FROM 
+    company_dim
+WHERE 
+    company_id IN (
+        SELECT
+            company_id
+        FROM 
+            job_postings_fact
+        WHERE 
+            job_no_degree_mention = TRUE
+    );
